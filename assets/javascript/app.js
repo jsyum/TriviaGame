1f55d99561cac006c3a234737e9ee148c4d01a97
jsyum <jennifersyum@gmail.com>
2019-02-15 16:36:13 -0500

    logging checked boxes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+
 //use jQuery to reveal the questions div on clicking "startbutton"
 $("#startbutton").click(show);
 
@@ -7,9 +11,20 @@ function show() {
   }
 }
 
-// document.getElementById("button1").addEventListener("click", function(){
+if (document.getElementById("china").checked == true) {
+  incorrect++;
+  showIncorrect();
+}
 
-//     document.getElementById("box").style.height = "250px";
-//     document.getElementById("box").style.width = "250px";
+console.log(incorrect);
 
-// });
+//functions to show correct, incorrect, and unanswered
+function showCorrect() {
+  document.getElementById("numcorrect").innerHTML = correct;
+}
+function showIncorrect() {
+  document.getElementById("numincorrect").innerHTML = incorrect;
+}
+function showUnanswered() {
+  document.getElementById("numunanswered").innerHTML = unanswered;
+}
