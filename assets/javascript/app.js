9790ea960aea34beb4f7d7500646f1a42984eca3
jsyum <jennifersyum@gmail.com>
2019-02-15 22:36:19 -0500

    first question logs result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,18 +5,45 @@ var unanswered = 0;
 //use jQuery to reveal the questions div on clicking "startbutton"
 $("#startbutton").click(show);
 
+//Show timer and questions div upon clicking startbutton
 function show() {
   if (document.getElementById("questionsdiv").style.display == "none") {
     document.getElementById("questionsdiv").style.display = "block";
+    runtimer();
   }
 }
 
-if (document.getElementById("china").checked == true) {
-  incorrect++;
-  showIncorrect();
+// Countdown timer
+var number = 60;
+var intervalId;
+function runtimer() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
+function decrement() {
+  number--;
+  $("#displaytimer").text(number);
 }
 
-console.log(incorrect);
+//Show correct and incorrect answers upon click
+$(document).ready(function() {
+  $("input[type=radio]").click(function() {
+    if (this.value == "Japan") {
+      correct++;
+      showCorrect();
+      alert("Good Job!");
+    } else {
+      incorrect++;
+      showIncorrect();
+    }
+  });
+});
+// function incorrect() {
+//   if (document.getElementById("china").checked == true) {
+//     incorrect++;
+//     showIncorrect();
+//   }
+// }
 
 //functions to show correct, incorrect, and unanswered
 function showCorrect() {
